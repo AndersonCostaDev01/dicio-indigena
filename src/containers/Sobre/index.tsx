f9ffb51961033eab99be8f4dab5ed7c080b8745e
Anderson Costa <costaandersom23@gmail.com>
2024-11-01 16:29:21 -0300

    Correção de erro

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -11,7 +11,7 @@ const Sobre = () => {
                 </TituloSobre>
                 <TextoSobre>
 
-                    As línguas indígenas no Brasil têm uma origem ancestral e são resultado da ContainerSobreersidade étnica dos povos nativos da América do Sul. Esses idiomas começaram a se desenvolver há milhares de anos, antes da chegada dos colonizadores, refletindo o modo de vida, cultura e tradições específicas de cada grupo. A variedade linguística indígena no Brasil é uma das mais ricas do mundo, e os idiomas indígenas estão ContainerSobreididos em famílias e troncos linguísticos que remontam a milênios de desenvolvimento independente e transmitem a cosmovisão e a história dos povos que as falam.
+                    As línguas indígenas no Brasil têm uma origem ancestral e são resultado da ContainerSobreersidade étnica dos povos nativos da América do Sul. Esses idiomas começaram a se desenvolver há milhares de anos, antes da chegada dos colonizadores, refletindo o modo de vida, cultura e tradições específicas de cada grupo. A variedade linguística indígena no Brasil é uma das mais ricas do mundo, e os idiomas indígenas estão em famílias e troncos linguísticos que remontam a milênios de desenvolvimento independente e transmitem a cosmovisão e a história dos povos que as falam.
                 </TextoSobre>
                 <ImagePrimary />
             </ContainerSobre>
@@ -21,7 +21,7 @@ const Sobre = () => {
                     Línguas Tupi-Guarani
                 </TituloSobre>
                 <TextoSobre>
-                    O tronco Tupi-Guarani é um dos mais antigos e difundidos na América do Sul. As línguas tupi-guarani derivam do tronco Tupi, que inclui várias línguas e dialetos falados desde o litoral até o interior. A língua Tupi era a mais falada na costa do Brasil na época do contato europeu, e sua influência é visível até hoje, com muitos vocábulos incorporados ao português brasileiro. As línguas tupi-guarani incluem ainda ContainerSobreersas variantes faladas por diferentes etnias, como o Guarani, o Nheengatu e o próprio Tupi Antigo, e são conhecidas por expressarem de maneira complexa a natureza e o meio ambiente, como as relações de parentesco e ciclos naturais.
+                    O tronco Tupi-Guarani é um dos mais antigos e difundidos na América do Sul. As línguas tupi-guarani derivam do tronco Tupi, que inclui várias línguas e dialetos falados desde o litoral até o interior. A língua Tupi era a mais falada na costa do Brasil na época do contato europeu, e sua influência é visível até hoje, com muitos vocábulos incorporados ao português brasileiro. As línguas tupi-guarani incluem ainda variantes faladas por diferentes etnias, como o Guarani, o Nheengatu e o próprio Tupi Antigo, e são conhecidas por expressarem de maneira complexa a natureza e o meio ambiente, como as relações de parentesco e ciclos naturais.
                 </TextoSobre>
                 </div>
                 <ImageTupi />
